901633b8b457486508e036e4c2882dab70a46a93
SErAphLi <seraphlivery@gmail.com>
2018-10-31 18:07:14 +0800

    disable logger

--- mcts.py
+++ mcts.py
@@ -3,13 +3,13 @@ import random
 
 
 class MCTS(object):
-    def __init__(self, side, search_size, logger):
+    def __init__(self, side, search_size, logger=None):
         self.tree_manager = TreeManager()
         self.tree_manager.c_uct = 2
         self.actual_side = side
         self.side = 1 - side
         self.search_size = search_size
-        self.logger = logger
+        # self.logger = logger
 
     def init(self, actions, env):
         self.root = MCTSNode(self.tree_manager, self.side,
@@ -17,64 +17,64 @@ class MCTS(object):
         self.env = env
 
     def uct(self):
-        self.logger.debug(f'=== UCT start === Step {self.env.game.step_count}')
-        self.logger.debug(f'game map {self.env.game.map}')
+        # self.logger.debug(f'=== UCT start === Step {self.env.game.step_count}')
+        # self.logger.debug(f'game map {self.env.game.map}')
         for i in range(self.search_size):
-            self.logger.debug('=== Search start ===')
+            # self.logger.debug('=== Search start ===')
             node = self.root
             env = self.env.clone()
             player_move = 2
-            self.logger.debug(f'game map {env.game.map}')
-            self.logger.debug(f'real side {self.actual_side}')
-            self.logger.debug(f'step {env.game.step_count}')
+            # self.logger.debug(f'game map {env.game.map}')
+            # self.logger.debug(f'real side {self.actual_side}')
+            # self.logger.debug(f'step {env.game.step_count}')
 
-            self.logger.debug('== Select ==')
+            # self.logger.debug('== Select ==')
             # Select
             while node.untried_actions == [] and node.children != []:
                 # node is fully expanded and non-terminal
                 node = node.select()
                 env.take_side_action(node.side, node.action)
-                self.logger.debug(f'Select side {node.side}, '
-                                  f'action {node.action}')
+                # self.logger.debug(f'Select side {node.side}, '
+                #                   f'action {node.action}')
                 player_move -= 1
                 if player_move == 0:
                     env.step()
-                    self.logger.debug('Select env step')
+                    # self.logger.debug('Select env step')
                     player_move = 2
 
-            self.logger.debug('== Expand ==')
+            # self.logger.debug('== Expand ==')
             # Expand
             if node.untried_actions:
                 # if we can expand (i.e. state/node is non-terminal)
                 action = random.choice(node.untried_actions)
                 env.take_side_action(1 - node.side, action)
-                self.logger.debug(f'Expand side {1 - node.side}, '
-                                  f'action {action}')
+                # self.logger.debug(f'Expand side {1 - node.side}, '
+                #                   f'action {action}')
                 player_move -= 1
                 if player_move == 0:
                     env.step()
-                    self.logger.debug('Expand env step')
+                    # self.logger.debug('Expand env step')
                     player_move = 2
                 node = node.add_node(1 - node.side, action,
                                      env.get_side_action(node.side))
-                self.logger.debug(f'Expand add node, side {node.side}, '
-                                  f'action {node.action}')
+                # self.logger.debug(f'Expand add node, side {node.side}, '
+                #                   f'action {node.action}')
             side = 1 - node.side
 
             roll_out = 0
-            self.logger.debug('== Rollout ==')
+            # self.logger.debug('== Rollout ==')
             # Rollout
             while env.get_side_action(side):
                 # while state is non-terminal
                 actions = env.get_side_action(side)
                 action = random.choice(actions)
                 env.take_side_action(side, action)
-                self.logger.debug(f'Rollout side {side}, action {action}')
+                # self.logger.debug(f'Rollout side {side}, action {action}')
                 side = 1 - side
                 player_move -= 1
                 if player_move == 0:
                     env.step()
-                    self.logger.debug('Rollout env step')
+                    # self.logger.debug('Rollout env step')
                     roll_out += 1
                     player_move = 2
 
@@ -85,7 +85,7 @@ class MCTS(object):
                     if env.get_reward(p, index) == -1:
                         reward = True
                     rewards.append(env.get_reward(p, index))
-            self.logger.debug(f'rewards {rewards}')
+            # self.logger.debug(f'rewards {rewards}')
             if roll_out < 2:
                 if reward:
                     2 == 3
@@ -94,7 +94,7 @@ class MCTS(object):
                 2 == 3
 
             winner = env.get_winner()
-            self.logger.debug(f'winner {winner}')
+            # self.logger.debug(f'winner {winner}')
             if winner >= 0:
                 if winner == node.side:
                     z = 1
@@ -103,17 +103,17 @@ class MCTS(object):
             else:
                 z = 0
 
-            self.logger.debug(f'z {z}')
+            # self.logger.debug(f'z {z}')
 
             # Backpropagate
             while node is not None:
                 node.update(z, 0)
-                self.logger.debug(f'side {node.side}, '
-                                  f'action {node.action}, z {z}')
+                # self.logger.debug(f'side {node.side}, '
+                #                   f'action {node.action}, z {z}')
                 node = node.parent
                 z *= -1
 
-            self.logger.debug('=== Search end ===')
+            # self.logger.debug('=== Search end ===')
 
         sorted_nodes = self.root.sorted_child()
 
--- mcts_demo.py
+++ mcts_demo.py
@@ -4,12 +4,13 @@ from util import init_logger
 
 
 def main():
-    logger = init_logger('mcts')
+    logger = None
+    # logger = init_logger('mcts')
     ai = [AI(0, 1000, logger), AI(1, 200, logger)]
     env = Env()
-    logger.debug(f'=== Game start ===')
+    # logger.debug(f'=== Game start ===')
     end = env.reset()
-    logger.debug(f'game map {env.game.map}')
+    # logger.debug(f'game map {env.game.map}')
 
     for p in [0, 1]:
         ai[p].init(env.get_side_action(p), env)
@@ -21,7 +22,7 @@ def main():
             a[p] = ai[p].uct()
         for p in [0, 1]:
             env.take_side_action(p, a[p])
-            logger.debug(f'game side {p}, action {a[p]}')
+            # logger.debug(f'game side {p}, action {a[p]}')
         end = env.step()
         for p in [0, 1]:
             if a[1 - p] in ai[p].root.action_child_map:
@@ -30,8 +31,8 @@ def main():
                 ai[p].root.up = None
             else:
                 ai[p].init(env.get_side_action(p), env)
-        logger.debug(f'game map {env.game.map}')
-        logger.debug(f'step {env.game.step_count}')
+        # logger.debug(f'game map {env.game.map}')
+        # logger.debug(f'step {env.game.step_count}')
         game_end = end[0]
 
     rewards = []
@@ -39,12 +40,12 @@ def main():
         for index in [0, 1]:
             rewards.append(env.get_reward(p, index))
 
-    logger.info(f'rewards {rewards}')
+    # logger.info(f'rewards {rewards}')
 
-    logger.info(f'step {env.game.step_count}')
-    logger.debug(f'base {env.game.map[4, 0]}, {env.game.map[4, 8]}')
-    logger.debug(f'=== Game end ===')
-    logger.info(f'winner {env.get_winner()}')
+    # logger.info(f'step {env.game.step_count}')
+    # logger.debug(f'base {env.game.map[4, 0]}, {env.game.map[4, 8]}')
+    # logger.debug(f'=== Game end ===')
+    # logger.info(f'winner {env.get_winner()}')
     env.save_replay('.')
 
 
